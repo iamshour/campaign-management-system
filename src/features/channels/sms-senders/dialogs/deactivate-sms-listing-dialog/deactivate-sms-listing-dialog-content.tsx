32df24c761d5158f8fdc7bc30c83feb19cff2783
Haifa Naim <haifa.naim@blue.ai>
2024-03-04 13:21:59 +0000

    Resolve CMS-815 "feature FE - U1.10 Deactivate a Sender ID Listing - UI LOGIC"

---
+++ src/features/channels/sms-senders/dialogs/deactivate-sms-listing-dialog/deactivate-sms-listing-dialog-content.tsx
@@ -0,0 +1,40 @@
+//#region Import
+import type { SmsListingType } from "@/features/channels/sms-senders/types"
+
+import { useDeactivateSmsListingMutation } from "@/features/channels/sms-senders/api"
+import { Button } from "@/ui"
+import toast from "react-hot-toast"
+import { useTranslation } from "react-i18next"
+//#endregion
+
+export interface DeactivateSmsListingDialogContentProps extends Pick<SmsListingType, "id"> {
+	/**
+	 * Callback function used to close the dialog
+	 */
+	closeDialog: () => void
+}
+
+const DeactivateSmsListingDialogContent = ({ closeDialog, id }: DeactivateSmsListingDialogContentProps) => {
+	const { t } = useTranslation("sms-senders", { keyPrefix: "dialogs.deactivateListingDialog" })
+
+	const [triggerDeactivateSmsListing, { isLoading }] = useDeactivateSmsListingMutation()
+
+	const deactivateListing = async () => {
+		await triggerDeactivateSmsListing(id).unwrap()
+
+		toast.success(t(`message.success`))
+
+		closeDialog()
+	}
+
+	return (
+		<div className='p-2'>
+			<p>{t("text")}</p>
+			<Button className='ms-auto mt-8 block px-12' disabled={isLoading} onClick={deactivateListing} type='button'>
+				{t("buttons.confirm")}
+			</Button>
+		</div>
+	)
+}
+
+export default DeactivateSmsListingDialogContent
