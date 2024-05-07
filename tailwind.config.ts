1b7cc2f28530096180e0b568804440c73409b60e
Ali Shour <iamshour@outlook.com>
2024-05-08 01:18:23 +0300

    added tw-presets - removed @types/redux-persist

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,35 +5,13 @@ import FormPlugin from "@tailwindcss/forms"
 import TypographyPlugin from "@tailwindcss/typography"
 import ScrollbarPlugin from "tailwind-scrollbar"
 import AnimatePlugin from "tailwindcss-animate"
-import plugin from "tailwindcss/plugin"
+import TwPresetsPlugin from "tw-presets"
 //#endregion
 
-const customUtilities = plugin(function ({ addUtilities }) {
-	addUtilities({
-		".backface-hidden": { "backface-visibility": "hidden" },
-		".backface-visible": { "backface-visibility": "visible" },
-		".flex-center": { alignItems: "center", display: "flex", justifyContent: "center" },
-		".inline-flex-center": { alignItems: "center", display: "inline-flex", justifyContent: "center" },
-		".prevent-selection": {
-			MozUserSelect: "none",
-			MsUserSelect: "none",
-			"user-select": "none",
-			WebkitTouchCallout: "none",
-			WebkitUserDrag: "none",
-			WebkitUserSelect: "none",
-		},
-		".transition-basic": {
-			transitionDuration: "300ms",
-			transitionProperty: "all",
-			transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-		},
-	})
-})
-
 const config: Config = {
-	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 	darkMode: ["class"],
-	plugins: [customUtilities, ScrollbarPlugin({ nocompatible: true }), FormPlugin, AnimatePlugin, TypographyPlugin],
+	plugins: [ScrollbarPlugin({ nocompatible: true }), FormPlugin, AnimatePlugin, TypographyPlugin, TwPresetsPlugin],
 	theme: {
 		container: {
 			center: true,
